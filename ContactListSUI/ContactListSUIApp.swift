e24b4db5a9c5aaa79fccf8479e57b68e522957a8
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-15 09:26:15 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Геннадий Ведерников on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
